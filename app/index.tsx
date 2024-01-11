fc6b3a7b9270060f93a0e318d4ce8351cf755875
Miguel Almeida <miguelalmeida845@gmail.com>
2024-01-11 16:03:29 +0000

    Configure typescript

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function Root() {
   return (
     <View>
-      <Text>Open up app/index.js to start working on your app!</Text>
+      <Text>Open up app/index.tsx to start working on your app!</Text>
     </View>
   );
 }
